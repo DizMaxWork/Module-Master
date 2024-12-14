3a6a94b67c1d23434813903c5ab34c11d707f7b0
DizMaxWork <sol.work@mail.ru>
2024-12-14 14:45:37 +0300

    Изменение картинок

--- src/components/Main/ModuleAndConfigOptions/ModuleAndConfigOptions.tsx
+++ src/components/Main/ModuleAndConfigOptions/ModuleAndConfigOptions.tsx
@@ -21,11 +21,7 @@ export const ModuleAndConfigOptions = ({
             {(options as WordsLibrary["moduleOptions"]).cards.map((card) => (
               <div className="bg-white rounded-3xl p-4 mb-6 2xl:w-auto xl:w-[24%] md:w-[47%] w-full">
                 <div className="overflow-hidden rounded-3xl">
-                  <img
-                    src={card.img}
-                    alt={card.title}
-                    className="w-full object-cover h-full transition ease-in-out hover:scale-110 hover:object-cover hover:w-full hover:h-full"
-                  />
+                  <img src={card.img} alt={card.title} />
                 </div>
                 <h3 className="text-xl text-totalBlack font-semibold pt-3">
                   {card.title}
@@ -47,11 +43,7 @@ export const ModuleAndConfigOptions = ({
             {(options as WordsLibrary["configurations"]).cards.map((card) => (
               <div className="bg-white rounded-3xl p-4 mb-6 2xl:w-auto xl:w-[32%] md:w-[47%] w-full">
                 <div className="overflow-hidden rounded-3xl">
-                  <img
-                    src={card.img}
-                    alt={card.title}
-                    className="w-full object-cover h-full transition ease-in-out hover:scale-110 hover:object-cover hover:w-full hover:h-full"
-                  />
+                  <img src={card.img} alt={card.title} />
                 </div>
                 <div className="flex justify-between items-center px-3">
                   <h3 className="text-xl text-totalBlack font-semibold pt-3">
